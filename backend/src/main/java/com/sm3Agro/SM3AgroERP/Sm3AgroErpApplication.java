3ad9300ce6abb296ffcb819411418552926e642a
AndreCarneiro00 <andre.carneiro.silva00@gmail.com>
2026-06-16 23:47:06 -0300

    initial commit

---
+++ backend/src/main/java/com/sm3Agro/SM3AgroERP/Sm3AgroErpApplication.java
@@ -0,0 +1,13 @@
+package com.sm3Agro.SM3AgroERP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sm3AgroErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sm3AgroErpApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sm3Agro/SM3AgroERP/Sm3AgroErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm3Agro.SM3AgroERP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sm3AgroErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
